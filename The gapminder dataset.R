73db3bf42dc0c83b359e0ce426fa6898c9cc4cca
slee <sanlee8495@gmail.com>
2023-06-28 20:26:56 -0700

    6/28/2023

--- The gapminder dataset.R
+++ The gapminder dataset.R
@@ -18,3 +18,20 @@ gapminder %>% mutate(lifeExpMonths = 12 * lifeExp)
 
 # Filter, mutate, and arrange the gapminder dataset
 gapminder %>% filter(year == 2007) %>% mutate(lifeExpMonths = 12 * lifeExp) %>% arrange(desc(lifeExpMonths))
+
+
+# Visualizing with ggplot2
+
+gapminder %>% filter(year == 1952) -> gapminder_1952
+
+ggplot(gapminder_1952, aes(x = gdpPercap, y = lifeExp)) +
+  geom_point()
+
+gapminder_1952 %>% ggplot2::ggplot(aes(x = pop, y = lifeExp)) + 
+  ggplot2::geom_point()
+
+gapminder_1952 %>% ggplot2::ggplot(aes(x = pop, y = lifeExp)) + 
+  ggplot2::geom_point() +
+  ggplot2::scale_x_log10() +
+  ggplot2::scale_y_log10() 
+
