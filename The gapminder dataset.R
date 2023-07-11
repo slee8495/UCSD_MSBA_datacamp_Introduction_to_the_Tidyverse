f844cdac18d2072001a8ba45177a76ed5761cbfc
slee <sanlee8495@gmail.com>
2023-07-10 22:58:56 -0700

    7/10/2023

--- The gapminder dataset.R
+++ The gapminder dataset.R
@@ -48,3 +48,72 @@ gapminder_1952 %>%
 ggplot(gapminder_1952, aes(x = pop, y = lifeExp, color = continent, size = gdpPercap)) +
   geom_point() +
   scale_x_log10()
+
+gapminder_1952 <- gapminder %>%
+  filter(year == 1952)
+
+gapminder_1952 %>% 
+  ggplot2::ggplot(aes(x = pop, y = lifeExp)) +
+  ggplot2::geom_point() +
+  ggplot2::scale_x_log10() +
+  ggplot2::facet_wrap(~continent)
+
+
+gapminder %>% 
+  ggplot2::ggplot(aes(x = gdpPercap, y = lifeExp, color = continent, size = pop)) +
+  ggplot2::geom_point() +
+  ggplot2::scale_x_log10() +
+  ggplot2::facet_wrap(~year)
+
+
+gapminder %>% 
+  dplyr::summarise(medianLifeExp = median(lifeExp))
+
+
+gapminder %>% 
+  dplyr::filter(year == 1957) %>% 
+  dplyr::summarise(medianLifeExp = median(lifeExp),
+                   maxGdpPercap = max(gdpPercap))
+
+gapminder %>% 
+  dplyr::filter(year == 1957) %>% 
+  dplyr::group_by(continent) %>% 
+  dplyr::summarise(medianLifeExp = median(lifeExp),
+                   maxGdpPercap = max(gdpPercap))
+
+
+gapminder %>% 
+  dplyr::group_by(continent, year) %>% 
+  dplyr::summarise(medianLifeExp = median(lifeExp),
+                   maxGdpPercap = max(gdpPercap))
+
+
+by_year <- gapminder %>%
+  group_by(year) %>%
+  summarize(medianLifeExp = median(lifeExp),
+            maxGdpPercap = max(gdpPercap))
+
+by_year %>% 
+  ggplot2::ggplot(aes(x = year, y = medianLifeExp)) +
+  ggplot2::geom_point() +
+  ggplot2::expand_limits(y = 0)
+
+gapminder %>% 
+  dplyr::group_by(continent, year) %>% 
+  dplyr::summarise(medianGdpPercap = median(gdpPercap)) -> by_year_continent
+
+by_year_continent %>% 
+  ggplot2::ggplot(aes(x = year, y = medianGdpPercap, color = continent)) +
+  ggplot2::geom_point() +
+  ggplot2::expand_limits(y = 0)
+
+
+gapminder %>% 
+  dplyr::filter(year == 2007) %>% 
+  dplyr::group_by(continent) %>% 
+  dplyr::summarise(medianLifeExp = median(lifeExp),
+                   medianGdpPercap = median(gdpPercap)) -> by_continent_2007
+
+by_continent_2007 %>% 
+  ggplot2::ggplot(aes(x = medianGdpPercap, y = medianLifeExp, color = continent)) +
+  ggplot2::geom_point()
